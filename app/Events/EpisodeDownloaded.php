3bdb1416c1aecdabebc235108fee244fcd851827
Ahmed Soliman <ahmedsoliman360@gmail.com>
2022-02-23 15:44:43 +0000

    Added the EpisodeDownloaded Event

---
+++ app/Events/EpisodeDownloaded.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class EpisodeDownloaded
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+    public $date;
+    public $episodeId;
+    public $podcastId;
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($date, $episodeId, $podcastId)
+    {
+        $this->date = $date;
+        $this->episodeId = $episodeId;
+        $this->podcastId = $podcastId;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
